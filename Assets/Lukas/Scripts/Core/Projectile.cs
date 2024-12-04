e18bc4012b6b5d4da8c9ad560a4153b602d43ab1
RDMJoker <45975936+RDMJoker@users.noreply.github.com>
2024-12-04 14:16:13 +0100

    Projectile, Enemy Spawner, Room completion framework
    - First baseline of room framework done
    - First base of healthsystem done
    - First base of enemy death logic done
    - First basic projectile integration
    - First basic framework for the room observer done and working

---
+++ Assets/Lukas/Scripts/Core/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using LL_Unity_Utils.Timers;
+namespace Lukas.Scripts.Core
+{
+    public class Projectile : MonoBehaviour
+    {
+        [SerializeField] float despawnTimerDuration;
+
+        Timer despawnTimer;
+        
+        //Only for debugging
+        [SerializeField] int damageValue;
+        
+        void Awake()
+        {
+            despawnTimer = new Timer(despawnTimerDuration);
+            despawnTimer.StartTimer();
+        }
+
+        void FixedUpdate()
+        {
+            if(despawnTimer.CheckTimer()) Destroy(gameObject);
+        }
+
+        void OnCollisionEnter(Collision _hit)
+        {
+            if(_hit.gameObject.TryGetComponent(out HealthSystem healthSystem))
+            {
+                healthSystem.TakeDamage(damageValue);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
