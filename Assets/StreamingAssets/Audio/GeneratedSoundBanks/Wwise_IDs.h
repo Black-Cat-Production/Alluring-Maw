ade6e9baa6802dbd56d62f029fa0e7f58bdbc5cb
bennyspon <bennysponholtz@gmail.com>
2026-08-05 14:14:30 +0200

    WWISE Integration

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,60 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_ENV_TORCH = 3621683420U;
+        static const AkUniqueID PLAY_ENV_TORCH2 = 2940525158U;
+        static const AkUniqueID PLAY_INTRO = 3103472528U;
+        static const AkUniqueID PLAY_MENU___DESCENT = 680859642U;
+        static const AkUniqueID PLAY_MENU___UI_LEARNSKILL1 = 3985305945U;
+        static const AkUniqueID PLAY_MENU___UI_MOUSEOVER_01 = 2121322094U;
+        static const AkUniqueID PLAY_MENU___UI_SELECT1 = 278397422U;
+        static const AkUniqueID PLAY_MENU___UI_SELECT2_01 = 2638794377U;
+        static const AkUniqueID PLAY_MENU_ATMO = 1905734753U;
+        static const AkUniqueID PLAY_MENU_MUSIC = 2228153899U;
+        static const AkUniqueID PLAY_PLAYER_ATTACK_LMC = 104602405U;
+        static const AkUniqueID PLAY_PLAYER_FS = 4078825889U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AUX = 983310469U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC_BUS = 3127962312U;
+        static const AkUniqueID SFX_BUS = 1502772432U;
+        static const AkUniqueID VOICE_BUS = 3472939855U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID ENV_BIG_ROOM = 2521112689U;
+        static const AkUniqueID ENV_CORRIDOR = 194525819U;
+        static const AkUniqueID ENV_SMALL_ROOM = 783969252U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
