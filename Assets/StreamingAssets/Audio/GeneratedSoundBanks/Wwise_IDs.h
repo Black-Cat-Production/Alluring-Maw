63fa0313fc8b62f7c557346c17b262369e71312c
bennyspon <bennysponholtz@gmail.com>
2026-08-07 16:01:31 +0200

    WWISE saved

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,12 +14,16 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_ATMO = 3133186549U;
+        static const AkUniqueID PLAY_BOSS_FS = 1075280761U;
+        static const AkUniqueID PLAY_BOSS_FS_CHAIN = 727530039U;
+        static const AkUniqueID PLAY_BOSS_IDLE_VOICE = 2050535857U;
+        static const AkUniqueID PLAY_BOSS_JUMP_STOMP = 2284332626U;
         static const AkUniqueID PLAY_ENV_BLUE_FIRE = 1849851393U;
         static const AkUniqueID PLAY_ENV_FIREBOWL = 1468104246U;
         static const AkUniqueID PLAY_ENV_TORCH = 3621683420U;
         static const AkUniqueID PLAY_ENV_TORCH2 = 2940525158U;
+        static const AkUniqueID PLAY_HEARTBEAT = 3765695918U;
         static const AkUniqueID PLAY_INTRO = 3103472528U;
-        static const AkUniqueID PLAY_KS_HEARTBEAT_1 = 243833269U;
         static const AkUniqueID PLAY_MENU_ATMO = 1905734753U;
         static const AkUniqueID PLAY_MENU_DESCENT = 190377696U;
         static const AkUniqueID PLAY_MENU_MUSIC = 2228153899U;
@@ -27,6 +31,7 @@ namespace AK
         static const AkUniqueID PLAY_MENU_UI_MOUSEOVER_01 = 3257834436U;
         static const AkUniqueID PLAY_MENU_UI_SELECT1 = 558031544U;
         static const AkUniqueID PLAY_MENU_UI_SELECT2_01 = 1590463151U;
+        static const AkUniqueID PLAY_PLATZHALTER = 3911744873U;
         static const AkUniqueID PLAY_PLAYER_ATTACK_LMC = 104602405U;
         static const AkUniqueID PLAY_PLAYER_DASH = 2175711460U;
         static const AkUniqueID PLAY_PLAYER_FS = 4078825889U;
@@ -38,6 +43,13 @@ namespace AK
         static const AkUniqueID PLAY_PLAYER_RMC_CHARGE1 = 4193784842U;
         static const AkUniqueID PLAY_PLAYER_RMC_CHARGE2 = 4193784841U;
         static const AkUniqueID PLAY_SPELL_HITMARKER = 1825965928U;
+        static const AkUniqueID PLAY_ZOMBIE_ATTACK = 3597927707U;
+        static const AkUniqueID PLAY_ZOMBIE_DIES = 635270414U;
+        static const AkUniqueID PLAY_ZOMBIE_FS = 895956798U;
+        static const AkUniqueID PLAY_ZOMBIE_HITMARKER = 818595436U;
+        static const AkUniqueID PLAY_ZOMBIE_IDLE_VOICE = 880339566U;
+        static const AkUniqueID SET_STATE_PAUSE = 2829022778U;
+        static const AkUniqueID STOP_HEARTBEAT = 3319673256U;
         static const AkUniqueID STOP_MENU = 2914981333U;
     } // namespace EVENTS
 
@@ -60,6 +72,10 @@ namespace AK
     namespace GAME_PARAMETERS
     {
         static const AkUniqueID HEALTH = 3677180323U;
+        static const AkUniqueID MASTER_VOLUME = 4179668880U;
+        static const AkUniqueID MUSIC_VOLUME = 1006694123U;
+        static const AkUniqueID SFX_VOLUME = 1564184899U;
+        static const AkUniqueID VOICE_VOLUME = 3538560642U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
@@ -70,10 +86,12 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID GAMEPLAY = 89505537U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MUSIC_BUS = 3127962312U;
         static const AkUniqueID REVERB = 348963605U;
         static const AkUniqueID SFX_BUS = 1502772432U;
+        static const AkUniqueID UI_SOUNDS = 2235087924U;
         static const AkUniqueID VOICE_BUS = 3472939855U;
     } // namespace BUSSES
 
